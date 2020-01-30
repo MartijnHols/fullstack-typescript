f7104ee555a3cf6dc4645f1da3041d7836fab3de
Martijn Hols <git@martijnhols.nl>
2020-01-30 18:06:56 +0100

    Fix Account primary key: add auto increment

--- backend/src/migrations/20200118221121-account_create.ts
+++ backend/src/migrations/20200118221121-account_create.ts
@@ -6,6 +6,7 @@ export function up(queryInterface: QueryInterface) {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
+      autoIncrement: true,
     },
     email: {
       type: DataTypes.STRING,
