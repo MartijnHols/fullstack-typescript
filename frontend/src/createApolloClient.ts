88ccd14fa0c50de0efe8f5b238282939f9c6c64d
Martijn Hols <git@martijnhols.nl>
2020-02-14 20:50:56 +0100

    Add WebSocketLink to ApolloClient

--- frontend/src/createApolloClient.ts
+++ frontend/src/createApolloClient.ts
@@ -1,11 +1,42 @@
-import ApolloClient from 'apollo-boost'
+import { split } from 'apollo-link'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import ApolloClient from 'apollo-client'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+import { OperationDefinitionNode } from 'graphql'
 
 import resolvers from './resolvers'
 
+const GRAPHQL_ENDPOINT = 'http://localhost:4000/graphql'
+const GRAPHQL_WEB_SOCKET_ENDPOINT = 'ws://localhost:4000/graphql'
+
+const httpLink = new HttpLink({
+  uri: GRAPHQL_ENDPOINT,
+})
+const wsLink = new WebSocketLink({
+  uri: GRAPHQL_WEB_SOCKET_ENDPOINT,
+  options: {
+    reconnect: true,
+  },
+})
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(
+      query,
+    ) as OperationDefinitionNode
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink,
+)
+
 const createApolloClient = () =>
   new ApolloClient({
-    uri: 'http://localhost:4000/graphql',
+    cache: new InMemoryCache(),
     resolvers,
+    link,
   })
 
 export default createApolloClient
